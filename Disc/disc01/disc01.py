713007cb78e85477c650c85f2abe281d3946ed19
mikanup <jinlu347@gmail.com>
2024-01-29 21:27:20 +0900

    updateByMon Jan 29 21:27:20 JST 2024

---
+++ Disc/disc01/disc01.py
@@ -0,0 +1,19 @@
+def wears_jacket_with_if(temp, raining):
+    """
+    >>> wears_jacket_with_if(90, False)
+    False
+    >>> wears_jacket_with_if(40, False)
+    True
+    >>> wears_jacket_with_if(100, True)
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if temp < 60:
+        return True
+    if raining == True:
+        return True
+    return False    
+
+print(wears_jacket_with_if(90, False))
+print(wears_jacket_with_if(40, False))
+print(wears_jacket_with_if(100, True))
